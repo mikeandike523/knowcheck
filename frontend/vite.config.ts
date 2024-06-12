89ed0e69faf8599dc7f446ba8dd71058ebffbe53
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-06-12 19:31:00 -0400

    progress

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
 import rewriteAll from "vite-plugin-rewrite-all";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert(), rewriteAll()],
-  server:{
-    // https: true
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
--- linkutils.sh
+++ linkutils.sh
@@ -4,4 +4,4 @@ rm -f backend/utils
 rm -f frontend/utils
 
 ln -s "$(pwd)/utils" "$(pwd)/backend/utils"
-ln -s "$(pwd)/utils" "$(pwd)/frontend/utils"
+ln -s "$(pwd)/utils" "$(pwd)/frontend/src/utils"
