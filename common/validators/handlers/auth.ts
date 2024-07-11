b361a3bb5763a3d5fe3c00a5d7604f051dc23bf3
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-07-11 13:22:14 -0400

    progress

--- common/validators/handlers/auth.ts
+++ common/validators/handlers/auth.ts
@@ -1,13 +1,22 @@
-import {z} from 'zod'
-import {TArgs} from "../../api-types/handlers/auth"
-import nonempty from '../../../utils/zod-refiners/nonempty'
+import { z } from "zod";
+import nonempty from "../../../utils/zod-refiners/nonempty";
 
-const validator = z.object({
-    subjectId:z.string().superRefine<string>(
-        nonempty(false,"Subject ID is required.")
-    )
-})
+export type TSchema = {
+  subjectId: string;
+  instanceId: string;
+  accessCode: string;
+};
 
-export const parse=(data:unknown)=>validator.parse(data) as TArgs
+const schema = {
+  subjectId: z
+    .string()
+    .superRefine<string>(nonempty(false, "Subject ID is required.")),
+  instanceId: z
+    .string()
+    .superRefine<string>(nonempty(false, "Instance ID is required.")),
+  accessCode: z
+    .string()
+    .superRefine<string>(nonempty(false, "Access code is required.")),
+};
 
-export default validator
+export default schema;
--- utils/input-validation.ts
+++ utils/input-validation.ts
@@ -1,6 +1,6 @@
-import {z, ZodError} from 'zod'
+import { z, ZodError } from "zod";
 
-import formatError from "./formatError"
+import formatError from "./formatError";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type SignificantValue = Exclude<any, null | undefined>;
@@ -22,7 +22,7 @@ export type ValidationResult<TData extends SignificantValue> = {
    * unknown client side error, so we can generate an extremely generic message
    * and then console.error the details for debugging
    */
-  errorSource?: "zod" | "unknown";
+  errorSource?: "zod" | "unknown" | "custom";
   /**
    * An option message or list of messages to display to the user.
    */
@@ -39,47 +39,221 @@ export type ValidationResult<TData extends SignificantValue> = {
  * retrieving it's DOM value will always be a string
  */
 export type Validator<TData extends SignificantValue> = (
-    value: string
-  ) => ValidationResult<TData>;
-
+  value: string,
+) => ValidationResult<TData>;
 
 /**
  * Wraps a zod validator into a simpler for appropriate for this app
  * This is generally because ZodError is a highly complex data type and we want to boild it down first
  */
 export function zodToSimple<TData extends SignificantValue>(
-    zodValidator: z.ZodType<TData>
-  ): Validator<TData> {
-    return (value: string): ValidationResult<TData> => {
-      try {
-        const result = zodValidator.safeParse(value);
-        if (result.success) {
-          return {
-            valid: true,
-            data: result.data
-          };
-        } else {
-          return {
-            valid: false,
-            errorSource: "zod",
-            message: result.error.errors.map((err) => err.message),
-          };
-        }
-      } catch (error) {
-        if (error instanceof ZodError) {
-          return {
-            valid: false,
-            errorSource: "zod",
-            message: error.errors.map((err) => err.message),
-          };
-        } else {
-          return {
-            valid: false,
-            errorSource: "unknown",
-            message: "An unknown error occurred",
-            extra: formatError(error),
-          };
+  zodValidator: z.ZodType<TData>,
+): Validator<TData> {
+  return (value: string): ValidationResult<TData> => {
+    try {
+      const result = zodValidator.safeParse(value);
+      if (result.success) {
+        return {
+          valid: true,
+          data: result.data,
+        };
+      } else {
+        return {
+          valid: false,
+          errorSource: "zod",
+          message: result.error.errors.map((err) => err.message),
+        };
+      }
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          valid: false,
+          errorSource: "zod",
+          message: error.errors.map((err) => err.message),
+        };
+      } else {
+        return {
+          valid: false,
+          errorSource: "unknown",
+          message: "An unknown error occurred",
+          extra: formatError(error),
+        };
+      }
+    }
+  };
+}
+
+/**
+ * @remarks
+ * "null" - explict null, must be null and only null, cannot be undefined or a value
+ * "undefined" - eplict undefined, must be undefined and only undefined, cannot be null or a vlue
+ */
+export type ApiInputRootType =
+  | "object"
+  | "array"
+  | "string"
+  | "boolean"
+  | "number"
+  | "null"
+  | "undefined";
+
+export interface CheckApiInputShapeOptions {
+  canBeNull: boolean;
+  canBeUndefined: boolean;
+  objectOptions?: {
+    fields?: string[];
+    strict?: boolean;
+  };
+  arrayOptions: {
+    /**
+    Useful if a tuple is expected
+    */
+    fixedLength?: number;
+  };
+}
+
+function createApiInputShapeChecker<TApiInput>(
+  rootType: ApiInputRootType,
+  {
+    canBeNull = false,
+    canBeUndefined = false,
+    objectOptions = {
+      /*
+       * Object must have at least the following fields
+       *
+       * The rule here is "Is it in Object.keys?",
+       * since a value that is present but undefined ay still be a valid value
+       */
+      fields: [],
+      /**
+       * If true, object may not have any more fields than those specified
+       *
+       * Once again, its based solely on Object.keys
+       */
+      strict: false,
+    },
+    arrayOptions = {
+      fixedLength: undefined,
+    },
+  }: CheckApiInputShapeOptions,
+) {
+  /**
+   * A shorecut to quickly define validation results in the error cases
+   */
+  const failResult = (
+    message: string,
+  ): ValidationResult<TApiInput | null | undefined> => {
+    return {
+      valid: false,
+      errorSource: "custom",
+      message,
+    };
+  };
+  const successResult = (
+    value: TApiInput | null | undefined,
+  ): ValidationResult<TApiInput | null | undefined> => {
+    return {
+      valid: true,
+      data: value,
+    };
+  };
+  const checkObjectFields = (
+    obj: object,
+  ): ValidationResult<TApiInput | null | undefined> => {
+    const foundKeys = Object.keys(obj);
+    const foundKeysSet = new Set(foundKeys);
+    const requiredKeys = objectOptions.fields ?? [];
+    const requiredKeysSet = new Set(requiredKeys);
+    const issues = [];
+    for (const key of requiredKeys) {
+      if (!foundKeysSet.has(key)) {
+        issues.push(`Missing key ${key}`);
+      }
+    }
+    if (objectOptions.strict) {
+      for (const key of foundKeys) {
+        if (!requiredKeysSet.has(key)) {
+          issues.push(`Extra key ${key}`);
         }
       }
+    }
+    return {
+      valid: true,
+      data: obj as TApiInput | null | undefined,
     };
-  }
+  };
+  return (value: unknown) => {
+    switch (rootType) {
+      // Handle easy cases first
+      case "undefined":
+        if (typeof value !== "undefined") {
+          return failResult(
+            "Api input must be undefined and only undefined, found type: " +
+              typeof value,
+          );
+        }
+        break;
+      case "null":
+        if (value !== null) {
+          if (typeof value === "object") {
+            return failResult(
+              "Api input must be explictly null. Recieved a regular javascript object.",
+            );
+          } else {
+            return failResult("Api must be explictly null.");
+          }
+        }
+        break;
+      case "number":
+      case "boolean":
+      case "string":
+        if (canBeUndefined && typeof value === "undefined") {
+          return successResult(value as TApiInput | undefined | null);
+        }
+        if (canBeNull && value === null) {
+          return successResult(value as TApiInput | undefined | null);
+        }
+        if (typeof value !== rootType) {
+          return failResult(
+            `Api input must be of type \"${rootType}\", got \"${typeof value}\"`,
+          );
+        }
+        break;
+      case "array":
+        if (typeof value !== "object") {
+          return failResult(
+            `Api input must be an array, got javascript type \"${typeof value}\"`,
+          );
+        }
+        if (!Array.isArray(value)) {
+          return failResult(
+            `Api input must be an array. Got a javascript object that is not an array.`,
+          );
+        }
+        if (typeof arrayOptions.fixedLength === "number") {
+          if ((value as Array<any>).length !== arrayOptions.fixedLength) {
+            return failResult(
+              `
+Api must be an array of length ${arrayOptions.fixedLength},
+got an array of length ${(value as Array<any>).length}`.trim(),
+            );
+          }
+        }
+        break;
+      case "object":
+        if (typeof value !== "object") {
+          return failResult(
+            `Api input must be a regular obejct, got javascript type \"${typeof value}\"`,
+          );
+        }
+        if (Array.isArray(value)) {
+          return failResult(
+            `Api input must be a regular object. Got an array.`,
+          );
+        }
+        return checkObjectFields(value as object);
+    }
+
+    return successResult(value as TApiInput | null | undefined);
+  };
+}
