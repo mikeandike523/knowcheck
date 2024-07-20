fb18f15ffeaa0b0c5e17a6edb3f175a33e39455e
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-07-20 18:45:07 -0400

    ts-migrate didnt work, but now considering simply using checkjs with tsconfig instead. I think ts-migrate needs an older version of eslint

--- utils/eslint.config.mjs
+++ utils/eslint.config.mjs
@@ -5,6 +5,7 @@ import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
